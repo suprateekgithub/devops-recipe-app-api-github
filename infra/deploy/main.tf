8ce6b81d771f1251fa9f15cae2916fc10b3490e0
Sups Demo <31506313+suprateekgithub@users.noreply.github.com>
2025-05-01 01:52:30 +0530

    added terraform

---
+++ infra/deploy/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket               = "supsdevops-recipe-app-tf-state"
+    key                  = "tf-state-deploy"
+    workspace_key_prefix = "tf-state-deploy-new"
+    region               = "ap-south-1"
+    encrypt              = true
+    dynamodb_table       = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/deploy"
+    }
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+}
+
+data "aws_region" "current" {}
+
